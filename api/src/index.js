882d7fb761b79ee29ca8ba54b814342c4bcf5105
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2026-02-11 18:05:22 +0500

    Add express & cors

--- api/src/index.js
+++ api/src/index.js
@@ -1,1 +1,13 @@
-console.log( process.env.PORT )
+import express from "express"
+import cors from "cors"
+
+const PORT = process.env.PORT
+const app = express()
+
+// Middlewares
+app.use( cors() )
+
+// Routes
+app.get( "/health", ( req, res ) => res.send( { status: "ok" } ) )
+
+app.listen( PORT, () => console.info( `API ready at: ${ PORT }` ) )
--- web/src/main.js
+++ web/src/main.js
@@ -1,1 +1,5 @@
 import "@css/main.css"
+
+fetch( "http://localhost:3000/health" )
+.then( res => res.json() )
+.then( json => console.log( json ) )
